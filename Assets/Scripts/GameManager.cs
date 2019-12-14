57222bc54e86bf609aa9a73d9f8dbe2a05f43f44
irachelz <ryhsz29@gmail.com>
2019-12-14 15:33:13 +0200

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static float MOVE_CONSTANT = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // all of the objects but the opening screen are not active
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // start with opening screen
+
+        // go to game and run game
+
+
+        //go to end screen
+        
+    }
+}
